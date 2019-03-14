f31dd9e96dc0a648743fba44a833b672de8284ba
pradhumnpanchal <pradhumn44@gmail.com>
2019-03-14 23:24:52 +0530

    not getting the build part.

---
+++ Day1/src/Day1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println("Started with configuring git with intelliJ "+str);
+    }
+}
